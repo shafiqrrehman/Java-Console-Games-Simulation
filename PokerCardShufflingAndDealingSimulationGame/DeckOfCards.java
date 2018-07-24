d9a6eea6a5b24ffc94408552ae9187ebc89909b8
shafiqrrehman <shafiq_1309e@aptechgdn.com>
2018-07-24 11:38:24 +0500

    DeckOfCards class represents a deck of playing card.

--- PokerCardShufflingAndDealingSimulationGame/DeckOfCards.java
+++ PokerCardShufflingAndDealingSimulationGame/DeckOfCards.java
@@ -1,12 +1,14 @@
+// DeckOfCards class represents a deck of playing cards.
 import java.util.Random;
 
 public class DeckOfCards {
 
-	private Card[] deck;
-	private int currentCard;
-	private static final int NUMBER_OF_CARDS = 52;
-	private static final Random randomNumbers = new Random();
+	private Card[] deck; // array of Card objects
+	private int currentCard; // index of next Card to be dealt
+	private static final int NUMBER_OF_CARDS = 52; // constant # of Cards
+	private static final Random randomNumbers = new Random(); // Random number genrator
 
+// Constructor fills deck of cards
 	public DeckOfCards() {
 
 		String[] faces = { "Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", 
@@ -14,33 +16,38 @@ public class DeckOfCards {
 		String[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
 
 		deck = new Card[ NUMBER_OF_CARDS ];
-		currentCard = 0;
+		currentCard = 0; // set currentCard so first card dealt to deck[0]
 
+		// Populate deck with Card Object
 		for ( int count = 0; count < deck.length; count++ ) {
 			
 			deck[ count ] = new Card( faces[count % 13], suits[count / 13] );
 		}
 	}
 
+// Shuffle deck of Cards with one-pass algorithm 
 	public void shuffle() {
 
 		currentCard = 0;
 
 		for ( int first = 0; first < deck.length; first++ ) {
 			
-			int second = randomNumbers.nextInt( NUMBER_OF_CARDS );
+			int second = randomNumbers.nextInt( NUMBER_OF_CARDS ); // select a random number between 0 to 51
 
+			// Swap current card with randomly selected card
 			Card temp = deck[ first ];
 			deck[ first ] = deck[ second ];
 			deck[ second ] = temp;
 		}
 	}
 
+	// Deal One Card
 	public Card dealCard() {
 
+		// Determine whether Cards remain to be dealt
 		if ( currentCard < deck.length )
 			return deck[ currentCard++ ];
 		else
-			return null;
+			return null; // return null to indicate that all Cards were dealt
 	}
 }
