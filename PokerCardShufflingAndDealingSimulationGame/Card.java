ffa535d5f3d7d54bc5e090b0ed142838683ba0e3
shafiqrrehman <shafiq_1309e@aptechgdn.com>
2018-07-24 11:38:09 +0500

    Card class represents a playing card.

--- PokerCardShufflingAndDealingSimulationGame/Card.java
+++ PokerCardShufflingAndDealingSimulationGame/Card.java
@@ -1,14 +1,17 @@
+// Card class represent a playing card.
 public class Card {
 
-	private String face;
-	private String suit;
+	private String face; // face of card ("Ace", "Deuce", .....)
+	private String suit; // suit of card ("Hearts", "Hearts", ....)
 
+	// two-argument constructor initializes card's face suit
 	public Card(String cardFace, String cardSuit) {
 
 		face = cardFace;
 		suit = cardSuit;
 	}
 
+	// return String representation of Card.
 	public String toString() {
 
 		return face + " of " + suit;
