f0ae3d4ae573c32f2b9a23e860fde60f0b1f9374
shafiqrrehman <shafiq_1309e@aptechgdn.com>
2018-07-24 11:38:46 +0500

    Card shuffling and dealing.

--- PokerCardShufflingAndDealingSimulationGame/DeckOfCardsTest.java
+++ PokerCardShufflingAndDealingSimulationGame/DeckOfCardsTest.java
@@ -1,15 +1,18 @@
+// Card shuffling and dealing
 public class DeckOfCardsTest {
 
+	// Execute the application in main method
 	public static void main(String[] args) {
 		
 		DeckOfCards myDeckOfCards = new DeckOfCards();
-		myDeckOfCards.shuffle();
+		myDeckOfCards.shuffle(); // place the Cards in random order
 
+		// Print all 52 Cards in order to in which they are dealt
 		for ( int i = 1; i <= 52; i++) {
 			
 			System.out.printf( "%-19s", myDeckOfCards.dealCard() );
 
-			if ( i % 4 == 0 )
+			if ( i % 4 == 0 ) // output newline after every fourth card
 				System.out.println();
 		}
 	}
